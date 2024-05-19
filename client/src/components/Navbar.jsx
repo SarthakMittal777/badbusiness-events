87abbe3377fbac2f84cb6c6389df6c98449e16bb
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2024-05-19 12:56:19 +0530

    slight change in dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             More
           </button>
           {isDropdownOpen && (
-            <div className="absolute right-0 mt-2 w-32 bg-white/85 border border-red-400 rounded-md shadow-lg">
+            <div className="absolute right-0 mt-2 w-32 bg-white/85 border border-red-400 rounded-md shadow-lg mr-4">
               <Link
                 to="/merchandise"
                 className="block px-3 py-2 text-red-700 font-medium hover:bg-red-100"
